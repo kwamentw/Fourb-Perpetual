b3b0dc65034e41c3330b7838be9f0ddc3f592c90
KWAME <antwi1496@hotmail.com>
2024-04-06 09:53:36 -0500

    Adding up to my perp

---
+++ src/PriceFeed.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier:MIT
+pragma solidity 0.8.20;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract pricefeed {
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+        );
+
+        (, int answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer);
+    }
+}
